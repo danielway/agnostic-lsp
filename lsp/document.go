2c18d1c37c9ba95d29ba6b557e14bac19184bec6
Daniel Way <projects@danieldway.com>
2023-01-16 19:41:31 -0600

    Add line position computation to document

--- lsp/document.go
+++ lsp/document.go
@@ -10,11 +10,13 @@ type document struct {
 	text   lsp.Document
 	ast    ast.Module
 	tokens []lexer.TokenMeta
+	lines  []int // \n character indices
 	server *server
 }
 
 func (d *document) process(client lsp.Client) {
-	ast, tokens, err := lexer.Parse(d.text.String())
+	text := d.text.String()
+	ast, tokens, err := lexer.Parse(text)
 	if err != nil {
 		_ = client.LogErrorf("failed to parse document: %w", err)
 		return
@@ -23,5 +25,22 @@ func (d *document) process(client lsp.Client) {
 	d.ast = ast
 	d.tokens = tokens
 
+	d.lines = make([]int, 0)
+	for pos, ch := range text {
+		if ch == '\n' {
+			d.lines = append(d.lines, pos)
+		}
+	}
+
 	_ = client.LogInfof("Document parsed/lexed. Module name: %s", ast.Name)
+}
+
+func (d *document) getCharLine(find int) int {
+	for line, pos := range d.lines {
+		if pos > find {
+			return line - 1
+		}
+	}
+
+	return len(d.lines) - 1
 }
